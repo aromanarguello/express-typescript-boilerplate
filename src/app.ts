d0dcbceeee72c30d8581e041201624183f46b949
aromanarguello <aromanarguello@gmail.com>
2022-08-24 15:33:55 -0400

    add index.ts boilerplate

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@ import cors from "cors";
 import express from "express";
 import httpStatus from "http-status";
 
-import config from "./config/config";
-import logger from "./config/logger";
 import morgan from "./config/morgan";
 import { errorConverter, errorHandler } from "./midlewares/error";
 import routes from "./routes/v1";
@@ -28,12 +26,8 @@ app.use((_, __, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
 });
 
-const PORT = config.port;
-
 app.use(errorConverter);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  logger.info(`Server started on port ${PORT}`);
-});
+export default app;
