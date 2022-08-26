38d964ff18b6c1222c6e49e79cb3fd21b6c1859d
aromanarguello <aromanarguello@gmail.com>
2022-08-25 22:43:25 -0400

    Remove left over auth middleware from signup route

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import authController from '../../controllers/auth.controller';
 import userDto from '../../dtos/user.dto';
-import authMiddleware from '../../midlewares/auth.middleware';
 import validateMiddleware from '../../midlewares/validate.middleware';
 
 const authRoute = express.Router();
 
-authRoute.post('/signup', authMiddleware, validateMiddleware(userDto.createUser), authController.signup);
+authRoute.post('/signup', validateMiddleware(userDto.createUser), authController.signup);
 authRoute.post('/login', validateMiddleware(userDto.loginUser), authController.login);
 
 export default authRoute;
